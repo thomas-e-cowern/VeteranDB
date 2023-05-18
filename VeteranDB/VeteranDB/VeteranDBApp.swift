751aa810546c4f6a95baed7b043654624d101c8e
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-05-18 10:26:59 -0400

    VeteranDB initial commit

---
+++ VeteranDB/VeteranDB/VeteranDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeteranDBApp.swift
+//  VeteranDB
+//
+//  Created by Thomas Cowern on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct VeteranDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
